e8910fb16cda02a08816fa3237f5e732159f572a
TONGYU LU <main@TLDR3.local>
2023-11-24 19:11:40 -0500

    Basic command parser functionality

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+mod nom_args;
+
+struct Handler {
+    command_prefix: String,
+}
+
+impl Handler {
+    fn new(command_prefix: String) -> Self { Self { command_prefix } }
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    // Set a handler for the `message` event - so that whenever a new message is received - the
+    // closure (or function) passed will be called.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple events can be dispatched
+    // simultaneously.
+    async fn message(&self, ctx: Context, msg: Message) {
+        let pref = &self.command_prefix[..];
+        if msg.content.len() >= pref.len() && &msg.content[..pref.len()] == pref {
+            // Sending a message can fail, due to a network error, an authentication error, or lack
+            // of permissions to post in the channel, so log to stdout when some error happens,
+            // with a description of it.
+            let content_tail = &msg.content[pref.len()..];
+            let reply = match nom_args::parse(content_tail) {
+                Ok(cmd) => format!("Received a command:\n```\n{:?}\n```", cmd),
+                Err(e) => format!("{}", e),
+            };
+            if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a shard is booted, and
+    // a READY payload is sent by Discord. This payload contains data like the current user's guild
+    // Ids, current user data, private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot. This will automatically prepend
+    // your bot token with "Bot ", which is a requirement by Discord for bot users.
+    let handler = Handler::new("nom/".into());
+    let mut client =
+        Client::builder(&token, intents).event_handler(handler).await.expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform exponential backoff until
+    // it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
---
+++ src/nom_args.rs
@@ -0,0 +1,254 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Arg {
+    Pos(String),
+    Kw(String, String),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Command {
+    name: String,
+    args: Vec<Arg>,
+    rest: String,
+}
+
+impl Command {
+    pub fn new(name: String, args: Vec<Arg>, rest: String) -> Self { Self { name, args, rest } }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseError {
+    loc: usize,
+    why: String,
+}
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "At position {} after prefix: {}", self.loc, self.why)
+    }
+}
+
+impl ParseError {
+    pub fn new(loc: usize, why: String) -> Self { Self { loc, why } }
+    pub fn max_by_loc(self, other: Self) -> Self {
+        if self.loc >= other.loc { self } else { other }
+    }
+}
+
+pub fn parse(s: &str) -> Result<Command, ParseError> {
+    let (name, mut s, mut offset) = expect_ident(s, 0)?;
+    let mut args = vec![];
+    loop {
+        if let Ok(s) = expect_rest(s, offset)
+            .or_else(|_| expect_spaces(s, offset).and_then(|(s, o)| expect_rest(s, o))) {
+            break Ok(Command::new(name, args, s))
+        }
+        let (s1, offset1) = expect_spaces(s, offset)?;
+        let (arg, s2, offset2) =
+            match expect_key_value(s1, offset1)
+                .map(|(k, v, r, o)| (Arg::Kw(k, v), r, o))
+                .or_else(
+                    |e| expect_value(s1, offset1)
+                    .map(|(v, r, o)| (Arg::Pos(v), r, o))
+                    .map_err(|e_| e.max_by_loc(e_))
+                )
+            {
+                Ok(x) => x,
+                Err(e) => break Err(e),
+            };
+        args.push(arg);
+        s = s2;
+        offset = offset2;
+    }
+}
+
+fn expect_rest(s: &str, offset: usize) -> Result<String, ParseError> {
+    if s.chars().count() == 0 {
+        Ok("".into())
+    } else if s.chars().nth(0).unwrap() == '/' {
+        Ok((&s[1..]).into())
+    } else {
+        Err(ParseError::new(offset, "expected a <rest> region beginning with '/'".into()))
+    }
+}
+
+fn expect_key_value(s: &str, offset: usize) -> Result<(String, String, &str, usize), ParseError> {
+    // eprintln!("expect_key_value({:?}, {:?})", s, offset);
+    expect_ident(s, offset).and_then(
+        |(k, s, offset)|
+            if s.chars().count() >= 1 && s.chars().nth(0).unwrap() == '=' {
+                expect_value(&s[1..], offset + 1).map(|(v, s, offset)| (k, v, s, offset))
+            } else {
+                Err(ParseError::new(offset, "expected '=' for key-value pair".into()))
+            }
+    )
+}
+
+fn expect_value(mut s: &str, mut offset: usize) -> Result<(String, &str, usize), ParseError> {
+    let mut buf = vec![];
+
+    loop {
+        match expect_string(s, offset) {
+            Ok((t, u, p)) => {
+                buf.push(t);
+                s = u;
+                offset = p;
+            }
+            Err(e) => break if buf.len() == 0 {
+                Err(e)
+            } else {
+                Ok((buf.join(""), s, offset))
+            }
+        }
+    }
+}
+
+fn expect_string(s: &str, offset: usize) -> Result<(String, &str, usize), ParseError> {
+    if s.chars().count() == 0 {
+        return Err(ParseError::new(offset, "missing string".into()));
+    }
+    let first = s.chars().nth(0).unwrap();
+
+    if first == '\'' || first == '"' {
+        expect_quoted_string(s, offset)
+    } else {
+        expect_naked_string(s, offset)
+    }
+}
+
+fn expect_quoted_string(s: &str, offset: usize) -> Result<(String, &str, usize), ParseError> {
+    let quo: char;
+    let mut end = 0usize;
+    let mut buf = String::new();
+    let mut itr = s.chars();
+    if let Some(c) = itr.next() {
+        if c == '\'' || c == '"' {
+            quo = c;
+            end += c.len_utf8();
+        } else {
+            return Err(ParseError::new(offset + end, "quoted string does not begin with quote".into()));
+        }
+    } else {
+        return Err(ParseError::new(offset + end, "missing string".into()));
+    }
+    while let Some(c) = itr.next() {
+        end += c.len_utf8();
+        if c == quo {
+            break;
+        } else if c != '\\' {
+            buf.push(c);
+        } else /* c == '\\' */ {
+            if let Some(d) = itr.next() {
+                end += d.len_utf8();
+                if d == '\'' || d == '"' || d == '\\' {
+                    buf.push(d);
+                } else if d == 'n' {
+                    buf.push('\n');
+                } else if d == 't' {
+                    buf.push('\t');
+                } else if d.is_whitespace() || d == '=' || d == '/' {
+                    return Err(ParseError::new(offset + end, "\\(whitespace), \\=, and \\/ are only available in naked strings".into()));
+                } else {
+                    return Err(ParseError::new(offset + end, format!("unsupported escape sequence \\{}", d)));
+                }
+            } else {
+                return Err(ParseError::new(offset + end, "unterminated quoted string".into()));
+            }
+        }
+    }
+
+    Ok((buf, &s[end..], offset + end))
+}
+
+fn expect_naked_string(s: &str, offset: usize) -> Result<(String, &str, usize), ParseError> {
+    let mut end = 0usize;
+    let mut buf = String::new();
+    let mut itr = s.chars();
+    while let Some(c) = itr.next() {
+        if !c.is_whitespace() && c != '=' && c != '\'' && c != '"' && c != '/' && c != '\\' {
+            end += c.len_utf8();
+            buf.push(c);
+        } else if c == '\\' {
+            end += c.len_utf8();
+            if let Some(d) = itr.next() {
+                end += d.len_utf8();
+                if d.is_whitespace() || d == '=' || d == '\'' || d == '"' || d == '/' || d == '\\' {
+                    buf.push(d);
+                } else if d == 'n' || d == 't' {
+                    return Err(ParseError::new(offset + end, "\\n and \\t are only available in quoted strings".into()));
+                } else {
+                    return Err(ParseError::new(offset + end, format!("unsupported escape sequence \\{}", d)));
+                }
+            } else {
+                return Err(ParseError::new(offset + end, "missing operand after escape character '\\'".into()));
+            }
+        } else {
+            if end == 0 {
+                return Err(ParseError::new(offset + end, format!("invalid character to appear in a naked string ({:?})", c)));
+            } else {
+                break;
+            }
+        }
+    }
+
+    Ok((buf, &s[end..], offset + end))
+}
+
+fn expect_ident(s: &str, offset: usize) -> Result<(String, &str, usize), ParseError> {
+    let end = s.chars()
+        .take_while(
+            |&c| ('0' <= c && c <= '9')
+                || ('A' <= c && c <= 'Z')
+                || ('a' <= c && c <= 'z')
+                || c == '_'
+                || c == '-'
+        )
+        .map(char::len_utf8).sum();
+
+    if end == 0 {
+        Err(ParseError::new(offset, "expected an identifier, which begins with [0-9A-Za-z_-]".into()))
+    } else {
+        Ok(((&s[..end]).into(), &s[end..], offset + end))
+    }
+}
+
+fn expect_spaces(s: &str, offset: usize) -> Result<(&str, usize), ParseError> {
+    let end = s.chars().take_while(|&c| c.is_whitespace()).map(char::len_utf8).sum();
+
+    if end == 0 {
+        Err(ParseError::new(offset, "expected whitespace".into()))
+    } else {
+        Ok((&s[end..], offset + end))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            parse("nom"),
+            Ok(Command::new("nom".into(), vec![], "".into())),
+        );
+        assert_eq!(
+            parse("clac -e/7 7 / 11 12 + +"),
+            Ok(Command::new("clac".into(), vec![Arg::Pos("-e".into())], "7 7 / 11 12 + +".into())),
+        );
+        assert_eq!(
+            parse("echo hello, world!"),
+            Ok(Command::new(
+                "echo".into(),
+                vec![Arg::Pos("hello,".into()), Arg::Pos("world!".into())],
+                "".into()
+            )),
+        );
+        assert_eq!(
+            parse("uwuify kaomoji-when=never/Euthanize me, sensei!"),
+            Ok(Command::new(
+                "uwuify".into(),
+                vec![Arg::Kw("kaomoji-when".into(), "never".into())],
+                "Euthanize me, sensei!".into()
+            )),
+        );
+    }
+}
